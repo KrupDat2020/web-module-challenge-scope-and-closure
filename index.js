22d222bd9b991a5d51d3ab14e091db735983dbc9
KrupDat2020 <mark.krupinsky@gmail.com>
2021-03-22 14:53:58 -0500

    working on baseball score per inning simulation

--- index.js
+++ index.js
@@ -65,9 +65,10 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    const score = Math.floor(Math.random() * 3);
 }
+inning();
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -84,8 +85,10 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, callback){
+  const numInnings = 0;
+numInnings++;
+console.log(innings);
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
