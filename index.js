aea4550556fee9da082d554e62f04bf06ed13050
KrupDat2020 <mark.krupinsky@gmail.com>
2021-03-22 13:29:03 -0500

    tasks 1 completed

--- index.js
+++ index.js
@@ -28,11 +28,14 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  counter1 is the closure function, counter2 is outside of the function
   
   2. Which of the two uses a closure? How can you tell?
-  
+  counter 1 uses closure
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     I think counter1 would be preferrable if noone else needs to call the counter2 function
 */
 
 // counter1 code
